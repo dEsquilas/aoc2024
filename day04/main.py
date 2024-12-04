1e47c8946ce5bf8053e9445ea3eacf846627d486
Dani Esquilas <desquilas@gmail.com>
2024-12-04 23:59:52 +0100

    Readability imporovement

--- day04/main.py
+++ day04/main.py
@@ -1,14 +1,17 @@
+from itertools import product
+
 def read_input(filename):
     lines = [list(line.strip()) for line in open(filename, "r")]
     return lines
+
 
 def day_3_p1(filename):
     matrix = read_input(filename)
 
     positions = [
-        [(0, 1),(0, 2),(0, 3)],
-        [(1, 0),(2, 0),(3, 0)],
-        [(1, 1),(2, 2),(3, 3)],
+        [(0, 1), (0, 2), (0, 3)],
+        [(1, 0), (2, 0), (3, 0)],
+        [(1, 1), (2, 2), (3, 3)],
         [(0, -1), (0, -2), (0, -3)],
         [(-1, 0), (-2, 0), (-3, 0)],
         [(-1, -1), (-2, -2), (-3, -3)],
@@ -18,19 +21,18 @@ def day_3_p1(filename):
 
     t = 0
 
-    for x in range(0, len(matrix)):
-        for y in range(0, len(matrix[x])):
-            for position in positions:
-                try:
-                    if (matrix[x][y] == 'X' and
+    for x, y in product(range(len(matrix)), range(len(matrix[0]))):
+        for position in positions:
+            try:
+                if (matrix[x][y] == 'X' and
                         matrix[validateIndex(x, position[0][0])][validateIndex(y, position[0][1])] == "M" and
                         matrix[validateIndex(x, position[1][0])][validateIndex(y, position[1][1])] == "A" and
                         matrix[validateIndex(x, position[2][0])][validateIndex(y, position[2][1])] == "S"
 
-                    ):
-                        t += 1
-                except Exception as e:
-                    pass
+                ):
+                    t += 1
+            except Exception as e:
+                pass
 
     return t
 
@@ -38,38 +40,40 @@ def day_3_p1(filename):
 def day_3_p2(filename):
     matrix = read_input(filename)
     positions = [
-        [(1, 1),(-1,-1)],
-        [(-1, -1),(1,1)],
-        [(1, -1),(-1,1)],
-        [(-1, 1),(1,-1)],
+        [(1, 1), (-1, -1)],
+        [(-1, -1), (1, 1)],
+        [(1, -1), (-1, 1)],
+        [(-1, 1), (1, -1)],
     ]
 
     t = 0
 
-    for x in range(0, len(matrix)):
-        for y in range(0, len(matrix[x])):
-            if matrix[x][y] == 'A':
-                founded = 0
-                for position in positions:
-                    try:
-                        if (matrix[validateIndex(x, position[0][0])][validateIndex(y, position[0][1])] == "M" and
+    for x, y in product(range(len(matrix)), range(len(matrix[0]))):
+        if matrix[x][y] == 'A':
+            founded = 0
+            for position in positions:
+                try:
+                    if (matrix[validateIndex(x, position[0][0])][validateIndex(y, position[0][1])] == "M" and
                             matrix[validateIndex(x, position[1][0])][validateIndex(y, position[1][1])] == "S"):
-                            founded += 1
+                        founded += 1
 
-                    except Exception as e:
-                        pass
-                if founded >= 2:
-                    t += 1
+                except Exception as e:
+                    pass
+            if founded >= 2:
+                t += 1
     return t
+
 
 def validateIndex(o, c):
     if o + c < 0:
         raise Exception("Invalid index")
     return o + c
 
+
 def test_day_3():
     assert day_3_p1("test.txt") == 18
     assert day_3_p2("test.txt") == 9
+
 
 test_day_3()
 
