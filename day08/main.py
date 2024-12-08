8afb1ad461dfb5e8815bf23d006311c127954e5a
Dani Esquilas <desquila@gmail.com>
2024-12-08 12:21:42 +0100

    Day 8

--- day08/main.py
+++ day08/main.py
@@ -2,24 +2,24 @@ from itertools import product
 
 def read_input(filename):
 	map = [[c for c in line] for line in open(filename).read().splitlines()]
-	antenas_by_type = {}
+	antennas_by_type = {}
 
 	for y, x in product(range(len(map)), range(len(map[0]))):
 		if map[y][x] != '.':
-			if map[y][x] not in antenas_by_type:
-				antenas_by_type[map[y][x]] = []
-			antenas_by_type[map[y][x]].append((y, x))
+			if map[y][x] not in antennas_by_type:
+				antennas_by_type[map[y][x]] = []
+			antennas_by_type[map[y][x]].append((y, x))
 
-	return antenas_by_type, len(map), len(map[0])
+	return antennas_by_type, len(map), len(map[0])
 
 def day_8_p1(filename):
 
-	antenas_by_type, h, w = read_input(filename)
+	antennas_by_type, h, w = read_input(filename)
 
 	antinodes_p1 = set()
 	antinodes_p2 = set()
 
-	for (key, antennas) in antenas_by_type.items():
+	for (key, antennas) in antennas_by_type.items():
 		for i in range(len(antennas)):
 			for j in range(i+1, len(antennas)):
 				current = antennas[i]
@@ -34,10 +34,25 @@ def day_8_p1(filename):
 				]
 
 				for n in potencial_antinodes:
-					if validate_node(n, w, h) and n not in antennas:
+					if validate_node(n, w, h) and n != current and n != next:
 						antinodes_p1.add(n)
 
-	return len(antinodes_p1), 34
+				antinodes_p2.add(current)
+				antinodes_p2.add(next)
+
+				for d in [0, 1]:
+					current_pt = antennas[i]
+					while True:
+						direction = 1 if d == 1 else -1
+						potencial_antinode = (
+							current_pt[0] + direction * director_vector[0],
+							current_pt[1] + direction * director_vector[1])
+						if not validate_node(potencial_antinode, w, h):
+							break
+						antinodes_p2.add(potencial_antinode)
+						current_pt = potencial_antinode
+
+	return len(antinodes_p1), len(antinodes_p2)
 
 def validate_node(node, width, height):
 	return 0 <= node[0] < height and 0 <= node[1] < width
@@ -47,6 +62,6 @@ def test_day_8():
 
 test_day_8()
 
-# p1, p2 = day_8_p1("input.txt")
-# print("Part 1: ", p1)
-# print("Part 2: ", p2)
+p1, p2 = day_8_p1("input.txt")
+print("Part 1: ", p1)
+print("Part 2: ", p2)
