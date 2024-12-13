ba5ca49c9847360bde14a39981d0befb2e4b7e40
Dani Esquilas <desquila@gmail.com>
2024-12-13 22:25:41 +0100

    Every day template

---
+++ template.py
@@ -0,0 +1,16 @@
+def read_input(filename):
+	return 0
+
+def day_XX_p1(filename):
+
+
+	return 0,0
+
+def test_day_XX():
+	assert day_XX("test.txt") == (0, 0)
+
+test_day_XX()
+
+p1, p2 = day_XX("input.txt")
+print("Part 1: ", p1)
+print("Part 2: ", p2)
